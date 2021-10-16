6a086d48bae0cbef731825ced85b1a19b4b656f5
ShivaTomar <shiva@datability.co>
2021-10-16 08:27:39 +0530

    v1.0.4 procfile added

--- src/main/java/com/spring/boot/server/security/config/AppSecurityConfig.java
+++ src/main/java/com/spring/boot/server/security/config/AppSecurityConfig.java
@@ -1,16 +1,9 @@
 package com.spring.boot.server.security.config;
 
-import com.spring.boot.server.security.authentication.AuthenticationProviderUserPassword;
 import com.spring.boot.server.security.filters.SecurityFilter;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
